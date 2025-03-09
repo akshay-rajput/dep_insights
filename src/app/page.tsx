00879e3aa116442ecc005ef44a0d37b60a9f1e54
Akshay <akshay@olvy.co>
2025-03-10 01:13:43 +0530

    remove - boilerplate from homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,7 @@
 import Link from 'next/link';
 
-export default function Home() {
-	const contentsMap = [
-		{
-			link: 'https://nextjs.org/',
-			title: '🚀 Next.js v15',
-			description:
-				'The latest version with app directory and server components.',
-		},
-		{
-			link: 'https://tailwindcss.com/',
-			title: '🎨 Tailwind CSS',
-			description: 'A utility-first CSS framework for rapid styling.',
-		},
-		{
-			link: 'https://ui.shadcn.com/docs',
-			title: '🧩 shadcn/ui',
-			description: 'Prebuilt, accessible, and customizable UI components.',
-		},
-		{
-			link: 'https://www.typescriptlang.org/',
-			title: '⚙️ TypeScript',
-			description: 'Strongly typed language for maintainable codebases.',
-		},
-		{
-			link: 'https://typicode.github.io/husky/',
-			title: '🛠️ Pre-Commit Hooks',
-			description:
-				'Husky pre-commit hook for formatting & type-checking staged files using tsc and Prettier.',
-		},
-	];
+export default async function Home() {
+	
 
 	return (
 		<div className='main-page-content'>
@@ -50,42 +22,6 @@ export default function Home() {
 				</p>
 			</div>
 
-			<div className='contents-of-template flex flex-col p-4 text-sm md:text-base mb-8'>
-				<h3 className='font-medium text-stone-500 mb-2'>
-					What&apos;s included in the Template?
-				</h3>
-				<ul className='w-full text-stone-400 flex flex-col gap-2'>
-					{contentsMap.map((content, index) => (
-						<li key={index} className=''>
-							{content.link ? (
-								<Link
-									href={content.link}
-									target='_blank'
-									className='text-amber-600 hover:text-amber-400'
-								>
-									{content.title}
-								</Link>
-							) : (
-								content.title
-							)}
-							: {content.description}
-						</li>
-					))}
-				</ul>
-
-				<div className='more-info my-6 leading-6 text-sm text-stone-400'>
-					For more information related to the template, and recommendations for
-					other libraries for common use-cases, check out the{' '}
-					<Link
-						href={'/about'}
-						target='_blank'
-						className='font-mono text-amber-600 hover:text-amber-400 underline decoration-wavy underline-offset-2 decoration-1 font-medium'
-					>
-						/about
-					</Link>{' '}
-					page.
-				</div>
-			</div>
 		</div>
 	);
 }
