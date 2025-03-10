6dcaedba47e68b8a56a459f8f873030954258360
Akshay <akshay@olvy.co>
2025-03-11 03:08:51 +0530

    fix - navbar github link

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -1,17 +1,15 @@
 'use client';
+import { LucideExternalLink } from 'lucide-react';
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
-import { usePathname } from 'next/navigation';
 
 export default function Navbar() {
-	const pathName = usePathname();
-
 	return (
-		<nav className='navbar w-full sticky top-0 backdrop-blur-md p-4 '>
+		<nav className='navbar w-full sticky top-0 z-20 backdrop-blur-md p-4 '>
 			<div className='w-full navbar-blurred-backdrop max-w-6xl mx-auto flex items-center justify-between'>
 				<div className='logo'>
-					<Link href='/'>
+					<Link href='/' title='Home'>
 						<Image
 							src='/logo.svg'
 							alt='Logo'
@@ -22,17 +20,14 @@ export default function Navbar() {
 					</Link>
 				</div>
 				<ul className='flex gap-4 items-center tracking-wider font-gochi-hand md:text-lg'>
-					<li
-						className={`${pathName.includes('about') ? 'text-amber-400 underline decoration-wavy underline-offset-2' : 'text-stone-400 hover:text-amber-400'}  `}
-					>
-						<Link href='/about'>about</Link>
-					</li>
-					<li className='text-stone-400 hover:text-amber-400'>
+					<li className='text-stone-400 hover:text-blue-400'>
 						<Link
-							href='https://github.com/akshay-rajput/next-jump-start'
+							href='https://github.com/akshay-rajput/dep_insights'
 							target='_blank'
+							className='flex items-center gap-2 text-sm'
 						>
-							github
+							Github
+							<LucideExternalLink size={14} />
 						</Link>
 					</li>
 				</ul>
