5b3b3ff926c9dcbc5d64ea9915bb590c26aba256
Akshay <akshay@olvy.co>
2025-03-11 03:09:52 +0530

    add - empty state for table

--- src/components/insights/VulnerabilitiesTable.tsx
+++ src/components/insights/VulnerabilitiesTable.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import { Vulnerability } from '@buf/safedep_api.bufbuild_es/safedep/messages/vulnerability/v1/vulnerability_pb';
 import {
 	Table,
@@ -8,6 +8,7 @@ import {
 	TableHeader,
 	TableRow,
 } from '@/components/ui/table';
+import { LucideScanHeart } from 'lucide-react';
 
 const VulnerabilityKeyMap: { [key: string]: string } = {
 	RISK_CRITICAL: 'Critical',
@@ -16,14 +17,19 @@ const VulnerabilityKeyMap: { [key: string]: string } = {
 	RISK_LOW: 'Low',
 };
 interface VulnerabilityTableProps {
-  vulnerabilities: Array<Vulnerability> | [];
+	vulnerabilities: Array<Vulnerability> | [];
 }
 
-export default function VulnerabilitiesTable({vulnerabilities}:VulnerabilityTableProps) {
-  return (
-    <div className='p-4 rounded ' id="vulnerabilities-table-container">
-      {vulnerabilities?.length ? (
-				<div className=' relative max-h-[300px] overflow-y-auto'>
+export default function VulnerabilitiesTable({
+	vulnerabilities,
+}: VulnerabilityTableProps) {
+	return (
+		<div
+			className='p-4 rounded flex flex-grow '
+			id='vulnerabilities-table-container'
+		>
+			{vulnerabilities?.length ? (
+				<div className=' relative max-h-[300px] flex-grow overflow-y-auto'>
 					<Table className='w-full relative overflow-hidden'>
 						<TableHeader className='sticky top-0 z-10 shadow-md'>
 							<TableRow>
@@ -35,10 +41,19 @@ export default function VulnerabilitiesTable({vulnerabilities}:VulnerabilityTabl
 						<TableBody className='overflow-y-auto'>
 							{vulnerabilities.map((vulnerability) => {
 								return (
-									<TableRow key={vulnerability.aliases[0].value || vulnerability.publishedAt} className=' overflow-x-auto'>
-										<TableCell className='font-medium'>{vulnerability.aliases[0].value}</TableCell>
+									<TableRow
+										key={vulnerability.aliases[0].value}
+										className=' overflow-x-auto'
+									>
+										<TableCell className='font-medium'>
+											{vulnerability.aliases[0].value}
+										</TableCell>
 										<TableCell>{vulnerability.summary}</TableCell>
-										<TableCell>{vulnerability.severities?.length ? VulnerabilityKeyMap[vulnerability.severities[0].risk] : '-'}</TableCell>
+										<TableCell>
+											{vulnerability.severities?.length
+												? VulnerabilityKeyMap[vulnerability.severities[0].risk]
+												: '-'}
+										</TableCell>
 									</TableRow>
 								);
 							})}
@@ -46,10 +61,15 @@ export default function VulnerabilitiesTable({vulnerabilities}:VulnerabilityTabl
 					</Table>
 				</div>
 			) : (
-				<div className='p-4 text-stone-400 rounded text-sm text-center w-full border border-gray-500'>
-					No vulnerabilities found to show in table
+				<div className='empty-state flex-grow border -m-4 border-gray-700 rounded-lg flex flex-col justify-center items-center'>
+					<span className='text-gray-600 text-4xl'>
+						<LucideScanHeart size={50} />
+					</span>
+					<span className='text-gray-500 text-sm mt-4'>
+						No vulnerabilities detected
+					</span>
 				</div>
 			)}
-    </div>
-  )
+		</div>
+	);
 }
